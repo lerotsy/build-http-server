382d157cf8755baba8304c2897ac5c38824f666f
lerotsy <rotsyrazafy@gmail.com>
2023-12-31 14:51:04 -0500

    handle concurrent requests

--- app/main.py
+++ app/main.py
@@ -1,29 +1,30 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
 from .request_handler import HttpRequestHandler
 
+def handle_client_connection(client_socket):
+    try:
+        data = client_socket.recv(1024)
+        handler = HttpRequestHandler(data)
+        client_socket.send(handler.process_request().encode())
+    except Exception as e:
+        print("Error handling request:", str(e))
+    finally:
+        client_socket.close()
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    print("Server is starting...")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client_socket, client_address = server_socket.accept() # wait for client
+    server_socket.listen()
 
-    
-    data = client_socket.recv(1024) 
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Connection from {client_address}")
 
-
-    try:
-        handler = HttpRequestHandler(data)
-        client_socket.send(handler.process_request().encode())  # Send the HTTP response
-        # print("Sent response: HTTP/1.1 200 OK")
-    except Exception as e:
-        print("Error sending response:", str(e))
-    finally:
-        client_socket.close()  # Close the client socket
+        client_thread = threading.Thread(target=handle_client_connection, args=(client_socket,))
+        client_thread.start()
 
 
 if __name__ == "__main__":
